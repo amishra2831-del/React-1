4e006a26583f8b4f6de468d556b6994eb90db142
sfghheyi5657 <amishra2831@gmail.com>
2026-07-08 14:42:01 +0530

    fix: adjust layout by adding missing div for button container

--- 04_bgchanger/src/App.jsx
+++ 04_bgchanger/src/App.jsx
@@ -12,6 +12,8 @@ function App() {
 
       <div className="fixed flex flex wrap justify-center bottom-12 inset-x-0 px-2">
 
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2  rounded-3xl">
+
         <button 
        onClick={() => setcolor("red")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
         style={{background: "Red"}}>Red</button>
@@ -28,6 +30,7 @@ function App() {
 
       </div>
 
+    </div>
     </div>
     </>
   )
